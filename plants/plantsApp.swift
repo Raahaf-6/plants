d82eee52fba02187de0035cfea0335535c16f8d9
Raahaf-6 <raahafyy@gmail.com>
2024-10-20 11:41:18 +0300

    Initial Commit

---
+++ plants/plantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantsApp.swift
+//  plants
+//
+//  Created by Rahaf on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct plantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plantsTests/plantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  plantsTests.swift
+//  plantsTests
+//
+//  Created by Rahaf on 20/10/2024.
+//
+
+import Testing
+@testable import plants
+
+struct plantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
